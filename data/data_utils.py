096dab0c8919d094878bbf29df16c3220228aca3
rothnic <nlr06886@gmail.com>
2014-12-22 08:28:50 -0600

    Numba/numpy based solution just about working. Somehow not applying the constraint on resting at the moment.

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,19 +2,34 @@ __author__ = 'nickroth'
 
 import pandas as pd
 import numpy as np
-import arrow
 import os
-from goal import T_REFERENCE
+
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 data_file = os.path.join(this_dir, 'data.h5')
 
+
 def load_toy_csv(csv_path):
+    """
+    Returns a dataframe for the csv.
+    :param csv_path: string of the path to the csv
+    :return: dataframe
+    """
     df = pd.DataFrame.from_csv(csv_path)
     return df
 
+
 def convert_data(csv_path):
+    """
+    Convert the given toy order csv file to a dataframe and store into HDF5 file for faster loading.
+    :param csv_path: string path for csv file
+    :return:
+    """
+
+    # open the HDF5 file
     store = pd.HDFStore(data_file)
+
+    # open the csv file
     print('Loading CSV File')
     df = load_toy_csv(csv_path)
     print('CSV File Loaded, Converting Dates/Times')
@@ -26,27 +41,22 @@ def convert_data(csv_path):
     df['Year'], df['Month'], df['Day'], df['Hour'], df['Minute'] = zip(*df['Arrival_time'].map(split_time))
 
     # parse time into number of minutes
-    df['minutes'] = pd.to_datetime(df['date_str'], format='%Y-%m-%d %H:%M:%S', unit='m').astype('int64') / \
-                    np.timedelta64(1, 'm').astype('timedelta64[ns]').astype('int64')
+    df['minutes'] = to_minutes(pd.to_datetime(df['date_str'], format='%Y-%m-%d %H:%M:%S', unit='m'))
 
     # pre-compute work hours
     df['Work_Hours'] = np.logical_and((df['Hour'].values < 19), (df['Hour'].values >= 9))
 
+    # calculate minutes relative to start of 2014
     df['Relative_minutes'] = df['minutes'] - df.ix[1, 'minutes']
 
-    #df['Rel_Arrival_time'] = (df['Arrival_time'] - T_REFERENCE.timestamp)/60.0
     print('Conversion Complete')
+
+    # save the dataframe into hdf5 store
     store['toy_orders'] = df
 
 
-def calc_rel_arrival(Year, Month, Day, Hour, Minute):
-    years = np.asarray([(str(yr) + '0101T000Z') for yr in Year.values.tolist()], dtype='datetime64[Y]')
-    months = Month.values.astype('timedelta64[M]')
-    days = Day.values.astype('timedelta64[D]')
-    hours = Hour.values.astype('timedelta64[h]')
-    minutes = Minute.values.astype('timedelta64[m]')
-    return (years + (months - np.timedelta64(1, 'M')) + (days - np.timedelta64(1, 'D')) + hours + minutes) - \
-           np.datetime64('2014-1-1')
+def to_minutes(the_datetimes):
+    return the_datetimes.astype('datetime64[m]').astype('int64')
 
 
 def convert_time(this_time):
@@ -55,14 +65,18 @@ def convert_time(this_time):
     :param this_row:
     :return:
     """
+
+    # split the given time by spaces
     times = this_time.split(' ')
-    time_components = ['Year', 'Month', 'Day', 'Hour', 'Minute']
+
+    # make sure the time has two digits
     if len(times[3]) == 1:
         times[3] = '0' + times[3]
 
     if len(times[4]) == 1:
         times[4] = '0' + times[4]
 
+    # build the valid time representation in a string format
     time_str = '-'.join(times[0:3]) + ' ' + times[3] + ':' + times[4] + ':00'
     return time_str
 
@@ -73,13 +87,31 @@ def split_time(this_time):
     :param this_row:
     :return:
     """
+
+    # split time by empty space
     times = this_time.split(' ')
+
+    # a list of times that are represented by the split times
     time_components = ['Year', 'Month', 'Day', 'Hour', 'Minute']
+
+    # iterate over the list to return an integer for each time component within a tuple
     return (int(times[idx]) for idx, time_component in enumerate(time_components))
 
+
 def load_toy_orders():
+    """
+    Returns the stored dataframe from the HDF5 storage.
+    :return: preprocessed toy order dataframe
+    """
+
+    # open the hdf5
     store = pd.HDFStore(data_file)
+
+    # return the toy orders dataframe, which is stored within it
     return store['toy_orders']
+
+
+START_MINUTE = to_minutes(np.datetime64('2014-01-01'))
 
 
 if __name__ == '__main__':
--- numba_workshop.py
+++ numba_workshop.py
@@ -2,59 +2,202 @@ __author__ = 'rothnic'
 
 import numpy as np
 from numba import jit
-from data.data_utils import load_toy_orders
+from data.data_utils import load_toy_orders, START_MINUTE, to_minutes
 import timeit
 
-# ToDo: precalculate key time periods ahead of time (sanctioned time)
 
 NUM_TOYS = 10000000
 NUM_ELVES = 900
 BATCH_CYCLE = 30  # minutes
+MINUTES_PER_DAY = 1440
+
+PROD_PENALTY = 0.9
+PROD_REWARD = 1.02
+MIN_PROD = 0.25
+MAX_PROD = 4.0
+
 
 def run_workshop():
-
     the_days = days('2014-01-01', 75)
     toy_orders = load_toy_orders()
 
-    toy_id = np.array((NUM_TOYS, 1))
-    toy_duration = np.array((NUM_TOYS, 1))
-    toy_duration_left = np.ones((NUM_TOYS, 1)) * 100
-    toy_arrival_min = np.array((NUM_TOYS, 1))
-    toy_processed = np.zeros((NUM_TOYS, 1), dtype=bool)
-    toy_finish = np.zeros((NUM_TOYS, 1))
+    toy_id = toy_orders.index
+    toy_duration = toy_orders['Duration'].values
+    toy_completion = np.zeros(NUM_TOYS)
+    toy_arrival_min = to_minutes(toy_orders['Relative_minutes'].values)
+    toy_processed = np.zeros(NUM_TOYS, dtype=bool)
+    batch_idx = np.zeros(NUM_TOYS, dtype=bool)
 
-    elf_id = np.array((NUM_ELVES, 1))
-    elf_working = np.array((NUM_ELVES, 1))
-    elf_working_until = np.array((NUM_ELVES, 1))
-    elf_resting = np.array((NUM_ELVES, 1))
-    elf_resting_complete = np.array((NUM_ELVES, 1))
-    elf_productivity = np.ones((NUM_ELVES, 1))
+    elf_working_until = np.zeros(NUM_ELVES)
+    elf_resting_left = np.zeros(NUM_ELVES)
+    elf_productivity = np.ones(NUM_ELVES)
+    eligible = np.zeros(NUM_ELVES, dtype=bool)
 
-    #while np.any(toy_duration_left > 0):
-    for i in xrange(np.size(the_days)):
+    i = 0
+    MAX_DAYS = np.size(the_days)
+    #MAX_DAYS = 5
+
+    # loop over each day
+    while i < MAX_DAYS and not np.all(toy_processed):
 
         the_day = the_days[i]
+        print(the_day)
+        print('Toys Left: ' + str(np.sum(np.logical_not(toy_processed))))
 
-        # Get all toys that haven't been processed and have arrived
-        batch_idx = get_batch(the_day, toy_arrival_min, toy_processed)
+        # loop over each hour
+        for j in xrange(0, 24):
+            the_hour = np.timedelta64(j, 'h')
+            working = work_hours(the_hour)
+            the_minute = to_minutes(the_day + the_hour) - START_MINUTE
 
-        if batch_idx is not None:
-            allocate_elfs(toy_id, toy_duration, elf_id, elf_working, elf_resting, elf_productivity, elf_productivity)
+            # if available elves and working and available toys
+            # if np.all(elf_resting_left > the_minute + 60):
+            #
+            #     # handle long resting periods to avoid the minute loop
+            #     update_resting(elf_resting_left, amount=60.0)
 
-        if work_hours():
-            update_resting()
+            if working:
 
-        update_work(toy_duration_left)
+                # update batch only when working
+                batch_idx = get_batch(the_minute, toy_arrival_min, toy_processed, batch_idx)
+
+                # loop over each minute so that we can continue to assign elves to short duration toys
+                for k in xrange(0, 60):
+                    the_minute += k
+
+                    update_resting(elf_resting_left, elf_working_until, the_minute)
+
+                    if np.any(batch_idx):
+
+                        # see if we have elves that can be used now
+                        if np.any(np.logical_and(elf_working_until < the_minute, elf_resting_left <= 0.0)):
+
+                                # get a pairing between valid elfs and toys, where we have a reference to their indices
+                            elf_indices, toy_indices = allocate_elfs(the_minute, batch_idx, toy_duration, elf_working_until,
+                                                                     elf_resting_left, elf_productivity, eligible)
+
+                            # set the times when these toys are complete
+                            build_toys(the_minute, elf_indices, elf_productivity, elf_working_until, toy_indices,
+                                       toy_duration, toy_completion, toy_processed)
+
+                            # figure out how long we must rest, and get the minutes that were worked in sanction/unsanctioned times
+                            print("" + str(the_day) + " " + str(the_hour) + " " + str(the_minute))
+                            sanctioned, unsanctioned = set_rest_minutes(the_hour, the_minute, elf_indices,
+                                                                        elf_working_until, elf_resting_left)
+
+                            # update the elf productivity based on when the toys were built
+                            update_productivity(elf_indices, elf_productivity, sanctioned, unsanctioned)
+
+                            # set toys from this batch as processed
+                            batch_idx[toy_indices] = False
+
+        i += 1
+
+    save_results()
+
+
+def save_results():
+    pass
 
 
 def days(start_year, num_years):
     start_year = np.datetime64(start_year)
     delta = np.timedelta64(1, 'D')
-    return np.array([start_year + delta*i for i in xrange(365*num_years)])
+    return np.array([start_year + delta * i for i in xrange(365 * num_years)])
 
 
-def work_hours():
-    return True
+def work_hours(the_hour):
+    return 9 <= the_hour < 19
+
+
+def update_productivity(elf_indices, elf_productivity, sanctioned, unsanctioned):
+    elf_productivity[elf_indices] = (elf_productivity[elf_indices] * (PROD_REWARD ** (sanctioned / 60.0)) *
+                                     (PROD_PENALTY ** (unsanctioned / 60.0)))
+    elf_productivity[elf_productivity < 0.25] = 0.25
+    elf_productivity[elf_productivity > 4.0] = 4.0
+
+
+# @jit
+def set_rest_minutes(the_hour, the_minute, elf_indices, working_until, resting_minutes):
+    """
+    Sets the time when the elf can start working again based on the duration they must work.
+    :param the_hour:
+    :param the_minute:
+    :param elf_indices:
+    :param resting_minutes:
+    :return:
+    """
+
+    resting_minutes[elf_indices] = 0.0
+    this_resting_minutes = resting_minutes[elf_indices]
+    sanctioned = np.zeros_like(this_resting_minutes)
+    unsanctioned = np.zeros_like(this_resting_minutes)
+    working_duration = working_until[elf_indices] - the_minute
+
+    # handle today's remaining time
+    this_minute = the_minute % 60
+    minutes_left = ((19 - the_hour.astype('int64')) * 60) - this_minute
+    # working_overtime = working_until > minutes_left
+
+
+    this_hour = the_hour
+
+    # working left within hours
+    finish_within_period = (working_duration - minutes_left.astype('int64')) < 0
+    within_hours = np.copy(working_duration)
+    within_hours[np.logical_not(finish_within_period)] = minutes_left
+    # Todo: detect where we finish within the hour batching period
+
+    # add to sanctioned working hours
+    sanctioned += within_hours
+
+    # remove the hours we worked
+    working_duration -= within_hours
+
+    # get the elves that start working overtime
+    still_working = working_duration > 0
+    if np.any(still_working):
+
+        periods = ['afternoon', 'morning', 'working']
+        durations = [300, 540, 600]
+
+        while np.any(still_working):
+            for i in range(len(periods)):
+                dur = durations[i]
+
+                # get the number we worked by either taking the time left, or difference in time left and length of the
+                # current working period that were are addressing
+                finish_within_period = (working_duration - dur) < 0
+                worked_hours = np.copy(working_duration)
+                worked_hours[np.logical_not(finish_within_period)] = dur
+
+                # remove the hours worked from the time left we need to work on the toys
+                working_duration[still_working] -= worked_hours[still_working]
+
+                # first two periods are unsanctioned hours, last is sanctioned
+                if i < 2:
+                    unsanctioned[still_working] += worked_hours[still_working]
+                else:
+                    sanctioned[still_working] += worked_hours[still_working]
+
+                # see if we are still working
+                still_working = working_duration > 0
+
+    return sanctioned, unsanctioned
+
+
+def build_toys(the_minute, elf_indices, elf_productivity, elf_working_until, toy_indices, toy_duration, toy_completion, toy_processed):
+    """
+    Sets the time when the toy completes construction, based on assigned elf's productivity and toy's build duration
+    :return:
+    """
+    ending_minute = the_minute + (toy_duration[toy_indices] / elf_productivity[elf_indices])
+
+    toy_completion[toy_indices] = ending_minute
+    toy_processed[toy_indices] = True
+
+    elf_working_until[elf_indices] = ending_minute
+    return toy_completion[toy_indices]
 
 
 def update_work(work_left, duration_left):
@@ -64,38 +207,73 @@ def update_work(work_left, duration_left):
     duration_left[duration_left_idx] -= 1
 
 
-def update_resting():
-    pass
+def update_resting(elf_resting_left, elf_working_until, the_minute, amount=1.0):
+    elf_resting_left[elf_working_until <= the_minute] -= amount
+    elf_resting_left[elf_resting_left < 0] = 0.0
 
 
-def get_batch(the_minute, arrival_minutes, toy_processed):
-    batch_idx = (arrival_minutes <= the_minute) and (toy_processed == False)
-    if not np.any(batch_idx):
-        batch_idx = None
+@jit
+def get_batch_fast(the_time, arrival_minutes, toy_processed, batch_idx):
+    """
+    Batches the indices of the unprocessed toys as they arrive.
+    :param the_time:
+    :param arrival_minutes:
+    :param toy_processed:
+    :param batch_idx:
+    :return:
+    """
+    return np.logical_and((arrival_minutes <= the_time), (toy_processed == False), batch_idx)
+
+
+def get_batch(the_time, arrival_minutes, toy_processed, batch_idx):
+    """
+    Batches the indices of the unprocessed toys as they arrive.
+    :param the_time:
+    :param arrival_minutes:
+    :param toy_processed:
+    :param batch_idx:
+    :return:
+    """
+    return get_batch_fast(the_time, arrival_minutes, toy_processed, batch_idx)
+
+
+@jit
+def get_batch_loop(the_time, arrival_minutes, toy_processed, batch_idx):
+    """
+    Get the current batch using a numba-compiled loop, vice numpy array operators
+    :param the_time:
+    :param arrival_minutes:
+    :param toy_processed:
+    :param batch_idx:
+    :return:
+    """
+    for i in range(batch_idx.size):
+        batch_idx[i] = arrival_minutes[i] <= the_time & toy_processed[i] == False
     return batch_idx
 
 
-def allocate_elfs(toy_ids, duration, elf_ids, working, working_until, resting, productivity):
-    eligible = (working == False) and (resting == False)
-    num_toys = len(toy_ids)
+def allocate_elfs(the_minute, batch_idx, duration, elf_working_until, elf_resting_left, productivity, eligible):
+    eligible = np.logical_and((elf_working_until <= the_minute), (elf_resting_left <= 0.0), eligible)
+    # elig_elf_indices = np.where(eligible)[0]
+    toy_indices = np.where(batch_idx)[0]
 
+    # get productivity of eligible elves
     elig_prod = productivity[eligible]
-    elig_id = elf_ids[eligible]
-    elig_duration = duration[eligible]
 
-    prod_sort = elig_prod.argsort()
-    prod_sort = prod_sort[:num_toys]
+    # only get number of toys as we have eligible elves
+    toy_indices = toy_indices[:np.sum(eligible)]
+    num_toys = toy_indices.size
 
-    elig_id = elig_id[prod_sort]
-    elig_prod = elig_prod[prod_sort]
+    elig_duration = duration[toy_indices]
 
+    # sort descending
+    prod_sort = elig_prod.argsort()[::-1]
+    elig_elf_indices = prod_sort[:min(num_toys, prod_sort.size)]
 
-
-
+    return elig_elf_indices, toy_indices
 
 
 run_workshop_jit = jit(run_workshop)
 
 if __name__ == "__main__":
-
     run_workshop()
